4f8f7239e59fa7221657558d8eee4ff1f4e1f9bf
knowave <kwaktaemin98@gmail.com>
2024-10-01 17:25:48 +0900

    Define BaseEntity

--- src/main/java/com/goalKeep/goalKeep/BaseEntity.java
+++ src/main/java/com/goalKeep/goalKeep/BaseEntity.java
@@ -1,4 +1,40 @@
 package com.goalKeep.goalKeep;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
 public class BaseEntity {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @CreationTimestamp()
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp()
+    @Column(insertable = true, updatable = true)
+    private LocalDateTime updatedAt;
+
+    @Column()
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onInsert() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreRemove
+    protected void onDelete() {
+        this.deletedAt = LocalDateTime.now();
+    }
 }
