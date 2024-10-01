99f39a02041faf46fddb81b9ab2312b262281843
knowave <kwaktaemin98@gmail.com>
2024-10-01 17:53:10 +0900

    user entity define

---
+++ src/main/java/com/goalKeep/goalKeep/domain/user/User.java
@@ -0,0 +1,54 @@
+package com.goalKeep.goalKeep.domain.user;
+
+import com.goalKeep.goalKeep.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity()
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false, length = 128)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(length = 255)
+    private String profileImage;
+
+    @Column(length = 100)
+    private String introduction;
+
+    @Column(length = 128)
+    private String token;
+
+    @Column()
+    private Boolean isDeleted = false;
+
+    @Column(nullable = false)
+    private int planCount = 0;
+
+    @Column(nullable = false)
+    private int completedPlanCount = 0;
+
+    @Column(nullable = false)
+    private int pendingPlanCount = 0;
+
+    @Column(nullable = false)
+    private int feedCount = 0;
+
+    @Column(nullable = false)
+    private int followerCount = 0;
+
+    @Column(nullable = false)
+    private int followingCount = 0;
+}
