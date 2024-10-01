aad9d61b8d87aa4b684fdcc405ce2cfcbe2df812
knowave <kwaktaemin98@gmail.com>
2024-10-01 13:27:43 +0900

    first commit

---
+++ src/main/java/com/goalKeep/goalKeep/GoalKeepApplication.java
@@ -0,0 +1,13 @@
+package com.goalKeep.goalKeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalKeepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goalKeep/goalKeep/GoalKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goalKeep.goalKeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
