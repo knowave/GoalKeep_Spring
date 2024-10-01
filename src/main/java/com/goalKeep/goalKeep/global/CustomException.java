a7700cd4608fca1ee7afe7722d5ad4c82dd6d61b
knowave <kwaktaemin98@gmail.com>
2024-10-01 18:27:46 +0900

    커스텀 예외처리 class

---
+++ src/main/java/com/goalKeep/goalKeep/global/CustomException.java
@@ -0,0 +1,26 @@
+package com.goalKeep.goalKeep.global;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    private final String message;
+    private final HttpStatus status;
+
+    public CustomException(String message, HttpStatus status) {
+        super(message);
+
+        this.message = message;
+        this.status = status;
+    }
+
+    @Override()
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
