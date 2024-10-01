a1620ac2cd88ee988b10d4289f7b6f95d7cf798c
knowave <kwaktaemin98@gmail.com>
2024-10-01 18:28:00 +0900

    user 생성 로직

--- src/main/java/com/goalKeep/goalKeep/domain/user/repository/UserRepository.java
+++ src/main/java/com/goalKeep/goalKeep/domain/user/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.goalKeep.goalKeep.domain.user.repository;
 
-public interface UserRepository {
+import com.goalKeep.goalKeep.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existByEmail(String email);
 }
--- src/main/java/com/goalKeep/goalKeep/domain/user/service/UserService.java
+++ src/main/java/com/goalKeep/goalKeep/domain/user/service/UserService.java
@@ -1,4 +1,25 @@
 package com.goalKeep.goalKeep.domain.user.service;
 
+import com.goalKeep.goalKeep.domain.user.dto.UserDto;
+import com.goalKeep.goalKeep.domain.user.repository.UserRepository;
+import com.goalKeep.goalKeep.global.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+    public void createUser(UserDto.Request createUserDto) {
+        if (userRepository.existByEmail(createUserDto.getEmail())) {
+            throw new CustomException("이미 존재하는 이메일입니다.", HttpStatus.BAD_REQUEST);
+        }
+
+        createUserDto.setPassword(encoder.encode(createUserDto.getPassword()));
+        userRepository.save(createUserDto.toEntity());
+    }
 }
