6bdca83a40021fa4308f974f1c42165e2257ab7f
knowave <kwaktaemin98@gmail.com>
2024-10-02 17:14:15 +0900

    .env file ignore

---
+++ src/main/java/com/goalKeep/goalKeep/domain/user/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.goalKeep.goalKeep.domain.user.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/goalKeep/goalKeep/infrastructure/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.goalKeep.goalKeep.infrastructure;
+
+public class SecurityConfig {
+}
