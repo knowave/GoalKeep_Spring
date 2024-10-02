f00316cb9df4ebe36890dbd4b29feb60cb4521d5
knowave <kwaktaemin98@gmail.com>
2024-10-02 17:31:20 +0900

    jpa 연결 및 dotenv file load

--- src/main/java/com/goalKeep/goalKeep/GoalKeepApplication.java
+++ src/main/java/com/goalKeep/goalKeep/GoalKeepApplication.java
@@ -1,12 +1,19 @@
 package com.goalKeep.goalKeep;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GoalKeepApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(GoalKeepApplication.class, args);
 	}
 
