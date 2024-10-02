91233686a675d575c600639a0eff1a8b79e9b71e
knowave <kwaktaemin98@gmail.com>
2024-10-02 17:30:22 +0900

    상속 매핑

--- src/main/java/com/goalKeep/goalKeep/BaseEntity.java
+++ src/main/java/com/goalKeep/goalKeep/BaseEntity.java
@@ -1,11 +1,16 @@
 package com.goalKeep.goalKeep;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseEntity {
     @Id()
     @GeneratedValue(strategy = GenerationType.UUID)
