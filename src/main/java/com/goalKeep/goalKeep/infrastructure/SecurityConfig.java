a7db87ca20a3d3cea628c5c3bc8f40094b00fd70
knowave <kwaktaemin98@gmail.com>
2024-10-23 17:31:44 +0900

    Spring security 기본 설정

--- src/main/java/com/goalKeep/goalKeep/infrastructure/SecurityConfig.java
+++ src/main/java/com/goalKeep/goalKeep/infrastructure/SecurityConfig.java
@@ -1,17 +1,87 @@
 package com.goalKeep.goalKeep.infrastructure;
 
+import com.goalKeep.goalKeep.global.provider.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtProvider jwtProvider;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtProvider jwtProvider) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtProvider = jwtProvider;
+    }
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        http
+                .csrf(auth -> auth.disable());
+
+        http
+                .formLogin(auth -> auth.disable());
+
+        http
+                .httpBasic(auth -> auth.disable());
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/login", "/", "join").permitAll()
+                                .anyRequest().authenticated()
+                        );
+
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration configuration = new CorsConfiguration();
+
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                configuration.setAllowedMethods(Collections.singletonList("*"));
+                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                configuration.setAllowCredentials(true);
+                configuration.setMaxAge(3600L);
+
+                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                return configuration;
+            }
+        };
     }
 }
